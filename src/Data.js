dfd4be1f02a1ee242a41d3b7e393c783854b08e0
Sathya248 <sathyakannan248@gmail.com>
2024-09-25 14:29:04 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id:1,
+        name:"Kaviya",
+        age:30,
+        image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-olly-733872.jpg&fm=jpg"
+    },
+
+    {
+        id:2,
+        name: "Banu",
+        age:31,
+        image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-olly-733872.jpg&fm=jpg"
+    },
+
+    {
+        id:3,
+        name:" Kalai",
+        age:32,
+        image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-olly-733872.jpg&fm=jpg"
+    },
+
+    {
+        id:4,
+        name: "Priya",
+        age:28,
+        image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-olly-733872.jpg&fm=jpg"
+    },
+    {
+        id:5,
+        name: "Pavithra",
+        age:33,
+        image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-olly-733872.jpg&fm=jpg"
+    },
+]
